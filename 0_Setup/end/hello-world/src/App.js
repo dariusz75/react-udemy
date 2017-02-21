f8681aa11035ebd96abcb9d0095d15aecf2e0e33
Dariusz Franczak <[darius32@op.pl]>
2017-02-21 16:20:57 +0000

    branch created

--- 0_Setup/end/hello-world/src/App.js
+++ 0_Setup/end/hello-world/src/App.js
@@ -6,16 +6,36 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <Content />
       </div>
     );
   }
 }
+
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+      )
+  }
+}
+
+
+class Content extends Component {
+  render() {
+    return (
+      <div>
+        <h1>TEST test</h1>
+        <p className="App-intro"> To get started, edit <code>src/App.js</code> and save to reload.</p>
+      </div>
+      )
+  }
+}
+
 
 export default App;
