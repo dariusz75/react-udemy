3ee20f0b3fba1397c2db6023b6cecd69945de49f
Dariusz Franczak <[darius32@op.pl]>
2017-02-22 11:52:41 +0000

    ex completed

--- 0_Setup/end/hello-world/src/App.js
+++ 0_Setup/end/hello-world/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <Content />
-        <Table />
+        <Clock />
       </div>
     );
   }
@@ -40,60 +40,44 @@ class Content extends Component {
   }
 }
 
-class Table extends Component {
+class Clock extends Component {
 
   constructor(props) {
     super(props);
     
-    this.state = {
-      data: [
-        {
-          'id': 1,
-          'name': 'Dariusz',
-          'age': 10 
-        },
-        {
-          'id': 2,
-          'name': 'Mariusz',
-          'age': 20 
-        },
-        {
-          'id': 3,
-          'name': 'Marek',
-          'age': 30 
-        }
-      ]
-    }
+    this.state = 
+      {
+        date: new Date(),
+        commentCount: 0
+      }
   }
 
-  render() {
-    return (
-      <table>
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Name</th>
-            <th>Age</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.data.map((person, i) => <TableRow key={i} data={person} />)}
-        </tbody>
-      </table>
-      )
-  }
-}
-
-class TableRow extends Component {
-  render() {
-    return (
-      <tr>
-        <td>{this.props.data.id}</td>
-        <td>{this.props.data.name}</td>
-        <td>{this.props.data.age}</td>
-      </tr>
+  tick () {
+    this.setState(
+      {
+        date: new Date(),
+        commentCount: this.state.commentCount + 1
+      }
     );
   }
+
+  componentDidMount() {
+    this.timeID = setInterval( () => this.tick(), 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timeID)
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>The time is: {this.state.date.toLocaleTimeString()} </h2>
+        <h3>{this.state.commentCount} seconds</h3>
+      </div>
+    );
+  }
+
 }
 
 export default App;
