616da88dbd1dcaeded44e5baa648f68eba63b107
TCSL\DariuszF <dariusz.franczak@taxcomputersystems.com>
2017-11-21 15:46:53 +0000

    03 finished

--- complete-react-web-developer/projects/indecision-app/public/scripts/app.js
+++ complete-react-web-developer/projects/indecision-app/public/scripts/app.js
@@ -1,10 +1,50 @@
 'use strict';
 
+// Template created as JSX - JavaScript XML
 var template = React.createElement(
-  'p',
-  null,
-  'This is JSX?'
+	'div',
+	null,
+	React.createElement(
+		'h1',
+		null,
+		'This is JSX!'
+	),
+	React.createElement(
+		'p',
+		null,
+		'This is some info'
+	)
 );
+
+// Template created as JSX expression
+var user = {
+	name: 'Mike',
+	age: 27,
+	location: 'New York'
+};
+
+var templateTwo = React.createElement(
+	'div',
+	null,
+	React.createElement(
+		'h1',
+		null,
+		user.name
+	),
+	React.createElement(
+		'p',
+		null,
+		'Age: ',
+		user.age
+	),
+	React.createElement(
+		'p',
+		null,
+		'Location: ',
+		user.location
+	)
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- complete-react-web-developer/projects/indecision-app/src/app.js
+++ complete-react-web-developer/projects/indecision-app/src/app.js
@@ -1,4 +1,28 @@
-var template = <p>This is JSX????</p>;
+// Template created as JSX - JavaScript XML
+var template = (
+	<div>
+		<h1>This is JSX!</h1>
+		<p>This is some info</p>
+	</div>
+);
+
+
+// Template created as JSX expression
+var user = {
+	name: 'Mike',
+	age: 27,
+	location: 'New York'
+}
+
+var templateTwo = (
+	<div>
+		<h1>{user.name}</h1>
+		<p>Age: {user.age}</p>
+		<p>Location: {user.location}</p>
+	</div>
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+
+ReactDOM.render(templateTwo, appRoot);
