233876f485ee2eee5276eb586815ce25f72d7148
TCSL\DariuszF <dariusz.franczak@taxcomputersystems.com>
2017-12-14 11:01:14 +0000

    comments added

--- complete-react-web-developer/projects/indecision-app/src/app.js
+++ complete-react-web-developer/projects/indecision-app/src/app.js
@@ -2,8 +2,7 @@ console.log('application is running');
 
 
 const app = {
-	title: 'Indecision App',
-	subtitle: 'Put your life in your hands',
+	title: 'Form Example',
 	options: []
 };
 
@@ -35,8 +34,7 @@ const render = () => {
 	const template = (
 		<div>
 			<h1>{app.title}</h1>
-			{app.subtitle && <p>{app.subtitle}</p>}
-			<p>{app.options.length > 0 ? 'Your options' : 'No options'}</p>
+			<p>{app.options.length > 0 ? 'Number of elements entered:' : 'No elements in array'}</p>
 			<p>{app.options.length}</p>
 			<button onClick={onRemoveAll}>Remove All</button>
 			<ol>
@@ -54,3 +52,48 @@ const render = () => {
 }
 
 render();
+
+/* 
+Aby 'ozywic' nasza forme uzyjemy zdarzenia onSubmit i za jego pomoca odwolamy sie do funkcji onFormSubmit.
+Nie wywolujemy funkcji a jedynie odwolujemy sie do niej.
+
+e.preventDefault() w funkcji sluzy do zignorowania zachowania domyslnego zdarzenia onSubmit.
+Dzieki temu zdarzeniowi mozemy przypisac wlasny kod.
+
+W pierwszej kolejnosci chcemy pobrac dane jezeli jakies zostana wprowadzone przez urzytkownika.
+posluzy nam do tego nastepujacy kod:
+const option = e.target.elements.option.value;
+
+gdzie:
+const option - jest stala przechowujaca wartosc podana przez urzytkownika
+
+e.target - odnosi sie do elementu na ktorym nasze zdzrzenie rozpoczyna dzialanie (w naszym przypadku element form)
+
+e.target.elements - odpowiada za dostep do wszystkich elementow formy posiadajacych wlasciwosc 'name'
+
+e.target.elements.option - odwolanie do elementu formy z atrybutem name="option" (w naszym przypadku jest to okno input)
+
+const option = e.target.elements.option.value - pobranie wartosci z okna input i przypisanie jej do stalej option
+
+--------------------------------------------------------------------------------------------------------------------------------
+
+Nastepnie w naszej funkcji onFormSubmit deklarujemy if statement.
+
+if (option) {
+		app.options.push(option);
+		e.target.elements.option.value = '';
+		render();
+	}
+	console.log(app.options);
+}
+
+Gdzie:
+if(option) - jezeli zostala wprowadzona  wartosc 
+
+app.options.push(option) - dodajemy ja do tablicy options znajdujacej sie w obiekcie app
+
+e.target.elements.option.value = '' - usuwamy wartosc z okna input po wprowadzeniu jej do tablicy
+
+render() - odpowiada za ponowne renderowanie strony
+
+*/
