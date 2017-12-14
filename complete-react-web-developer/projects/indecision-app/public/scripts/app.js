05424b5f5bd82289f572c34d050d28c2f188570d
TCSL\DariuszF <dariusz.franczak@taxcomputersystems.com>
2017-12-14 14:06:03 +0000

    push commit

--- complete-react-web-developer/projects/indecision-app/public/scripts/app.js
+++ complete-react-web-developer/projects/indecision-app/public/scripts/app.js
@@ -3,36 +3,13 @@
 console.log('application is running');
 
 var app = {
-	title: 'Indecision App',
+	title: 'Toggle Button',
 	options: []
 };
 
-var onFormSubmit = function onFormSubmit(e) {
-	e.preventDefault();
-
-	var option = e.target.elements.option.value;
-	var keyValue = 0;
-	;
-	if (option) {
-		app.options.push(option);
-		e.target.elements.option.value = '';
-		keyValue = keyValue + 1;
-		render();
-	}
-	console.log(app.options);
-};
-
-var onRemoveAll = function onRemoveAll() {
-	app.options = [];
-	console.log(app.options);
+var onToggleButton = function onToggleButton() {
+	console.log('test');
 	render();
-};
-
-var onMakeDecision = function onMakeDecision() {
-	var randomArayIndex = Math.floor(Math.random() * app.options.length);
-	var randomOption = app.options[randomArayIndex];
-	console.log(randomArayIndex);
-	console.log(randomOption);
 };
 
 var appRoot = document.getElementById('app');
@@ -48,40 +25,9 @@ var render = function render() {
 			app.title
 		),
 		React.createElement(
-			'p',
-			null,
-			app.options.length > 0 ? 'Your options' : 'No options'
-		),
-		React.createElement(
 			'button',
-			{ disabled: app.options.length === 0, onClick: onMakeDecision },
-			'Log random table element'
-		),
-		React.createElement(
-			'button',
-			{ onClick: onRemoveAll },
-			'Remove All'
-		),
-		React.createElement(
-			'ol',
-			null,
-			app.options.map(function (option) {
-				return React.createElement(
-					'li',
-					{ key: option },
-					option
-				);
-			})
-		),
-		React.createElement(
-			'form',
-			{ onSubmit: onFormSubmit },
-			React.createElement('input', { type: 'text', name: 'option' }),
-			React.createElement(
-				'button',
-				null,
-				'Add Option'
-			)
+			{ disabled: false, onClick: onToggleButton },
+			'Show info'
 		)
 	);
 
@@ -91,111 +37,6 @@ var render = function render() {
 render();
 
 /* 
-Zadaniem tego cwiczenia jest pobranie przypadkowej wartosci z tablicy.
-
-Posluzy nam do tego bardzo prosta funkcja:
-
-const onMakeDecision = () => {
-	const randomArayIndex = Math.floor(Math.random() * app.options.length);
-	const randomOption = app.options[randomArayIndex];
-	console.log(randomArayIndex);
-	console.log(randomOption);
-}
-
-
-Gdzie:
-const randomArayIndex = Math.floor(Math.random() * app.options.length) - pobiera losowo index tablicy
-const randomOption = app.options[randomArayIndex] - pobiera losowo element tablicy wskazujac go poprzez losowo wbrany index
-
-Dodatkowo wylaczylismy przycisk 'Log random table element' kiedy tablica nie posiada zadnego elementu.
-<button disabled={app.options.length === 0} onClick={onMakeDecision}>Log random table element</button>
-
-
-
-
-
-
-
-##################### objasnienia poprzedniego cwiczenia #########################
-Zadaniem tego cwiczenia jest wyswietlenie listy wartosci tablicy jako zbior elementow <ul>
-Wykonamy to za pomoca wbudowanej metody map()
-
-Nasz kod wygladal bedzie nastepujaco:
-<ol>
-	{
-		app.options.map(function(option){
-			return (<li key={option}>{option}</li>);
-		})
-	}
-</ol>
-
-Gdzie:
-app.options.map - metoda map zostaje uruchomiona na tablicy options, ktora znajduje sie w obiekcie app.
-
-Metoda map() itteruje przez tablice i zwraca nowa tablice z tymi samymi wartosciami lub z wartosciami zmodyfikowanymi w podany przez nas sposob.
-Sposob modyfikacji okreslamy w funkcji ktora jest parametrem metody map()
-W naszym przypadku jest to:
-
-function(option){
-	return (<li key={option}>{option}</li>);
-}
-
-Gdzie:
-parametr 'option' to dowolna nazwa do ktorej przypiszemy wartosc elementu tablicy.
-
-Jak widzimy w powyzszymm przykladzie, funkcja zwraca element <li> posiadajacy wartosc elementu tablicy.
-Chcac zapetlic dzialane funkcji i zwrot wszystkich wartosci tablicy, musimy okreslic atrybut 'key' z unikalna wartoscia.
-Jest to wewnetrzny wymog Reacta.
-W naszym przykladzie przypisujemy wartosc elementu tablicy. To rozwiazanie nie jest wlasciwe poniewaz tablica moze zawierac elementy o takich samych wartosciach. Do naszego cwiczenia, jednak, wystarczy.
-
-
-
-
-
-
-
-##################### objasnienia poprzedniego cwiczenia #########################
-
-Aby 'ozywic' nasza forme uzyjemy zdarzenia onSubmit i za jego pomoca odwolamy sie do funkcji onFormSubmit.
-Nie wywolujemy funkcji a jedynie odwolujemy sie do niej.
-
-e.preventDefault() w funkcji sluzy do zignorowania zachowania domyslnego zdarzenia onSubmit.
-Dzieki temu zdarzeniowi mozemy przypisac wlasny kod.
-
-W pierwszej kolejnosci chcemy pobrac dane jezeli jakies zostana wprowadzone przez urzytkownika.
-posluzy nam do tego nastepujacy kod:
-const option = e.target.elements.option.value;
-
-gdzie:
-const option - jest stala przechowujaca wartosc podana przez urzytkownika
-
-e.target - odnosi sie do elementu na ktorym nasze zdzrzenie rozpoczyna dzialanie (w naszym przypadku element form)
-
-e.target.elements - odpowiada za dostep do wszystkich elementow formy posiadajacych wlasciwosc 'name'
-
-e.target.elements.option - odwolanie do elementu formy z atrybutem name="option" (w naszym przypadku jest to okno input)
-
-const option = e.target.elements.option.value - pobranie wartosci z okna input i przypisanie jej do stalej option
-
---------------------------------------------------------------------------------------------------------------------------------
-
-Nastepnie w naszej funkcji onFormSubmit deklarujemy if statement.
-
-if (option) {
-		app.options.push(option);
-		e.target.elements.option.value = '';
-		render();
-	}
-	console.log(app.options);
-}
-
-Gdzie:
-if(option) - jezeli zostala wprowadzona  wartosc 
-
-app.options.push(option) - dodajemy ja do tablicy options znajdujacej sie w obiekcie app
-
-e.target.elements.option.value = '' - usuwamy wartosc z okna input po wprowadzeniu jej do tablicy
-
-render() - odpowiada za ponowne renderowanie strony
+Zadaniem tego cwiczenia jest 
 
 */
--- complete-react-web-developer/projects/indecision-app/src/app.js
+++ complete-react-web-developer/projects/indecision-app/src/app.js
@@ -2,37 +2,16 @@ console.log('application is running');
 
 
 const app = {
-	title: 'Indecision App',
+	title: 'Toggle Button',
 	options: []
 };
 
-const onFormSubmit = (e) => {
-	e.preventDefault();
 
-	const option = e.target.elements.option.value;
-	let keyValue = 0;
-	;
-	if (option) {
-		app.options.push(option);
-		e.target.elements.option.value = '';
-		keyValue = keyValue +1;
-		render();
-	}
-	console.log(app.options);
-}
-
-const onRemoveAll = () => {
-	app.options = [];
-	console.log(app.options);
+const onToggleButton = () => {
+	console.log('test');
 	render();
 }
 
-const onMakeDecision = () => {
-	const randomArayIndex = Math.floor(Math.random() * app.options.length);
-	const randomOption = app.options[randomArayIndex];
-	console.log(randomArayIndex);
-	console.log(randomOption);
-}
 
 const appRoot = document.getElementById('app');
 
@@ -42,20 +21,7 @@ const render = () => {
 	const template = (
 		<div>
 			<h1>{app.title}</h1>
-			<p>{app.options.length > 0 ? 'Your options' : 'No options'}</p>
-			<button disabled={app.options.length === 0} onClick={onMakeDecision}>Log random table element</button>
-			<button onClick={onRemoveAll}>Remove All</button>
-			<ol>
-				{
-					app.options.map(function(option){
-						return (<li key={option}>{option}</li>);
-					})
-				}
-			</ol>
-			<form onSubmit={onFormSubmit}>
-				<input type="text" name="option"/>
-				<button>Add Option</button>
-			</form>
+			<button disabled={false} onClick={onToggleButton}>Show info</button>
 		</div>
 	);
 
@@ -67,111 +33,6 @@ render();
 
 
 /* 
-Zadaniem tego cwiczenia jest pobranie przypadkowej wartosci z tablicy.
-
-Posluzy nam do tego bardzo prosta funkcja:
-
-const onMakeDecision = () => {
-	const randomArayIndex = Math.floor(Math.random() * app.options.length);
-	const randomOption = app.options[randomArayIndex];
-	console.log(randomArayIndex);
-	console.log(randomOption);
-}
-
-
-Gdzie:
-const randomArayIndex = Math.floor(Math.random() * app.options.length) - pobiera losowo index tablicy
-const randomOption = app.options[randomArayIndex] - pobiera losowo element tablicy wskazujac go poprzez losowo wbrany index
-
-Dodatkowo wylaczylismy przycisk 'Log random table element' kiedy tablica nie posiada zadnego elementu.
-<button disabled={app.options.length === 0} onClick={onMakeDecision}>Log random table element</button>
-
-
-
-
-
-
-
-##################### objasnienia poprzedniego cwiczenia #########################
-Zadaniem tego cwiczenia jest wyswietlenie listy wartosci tablicy jako zbior elementow <ul>
-Wykonamy to za pomoca wbudowanej metody map()
-
-Nasz kod wygladal bedzie nastepujaco:
-<ol>
-	{
-		app.options.map(function(option){
-			return (<li key={option}>{option}</li>);
-		})
-	}
-</ol>
-
-Gdzie:
-app.options.map - metoda map zostaje uruchomiona na tablicy options, ktora znajduje sie w obiekcie app.
-
-Metoda map() itteruje przez tablice i zwraca nowa tablice z tymi samymi wartosciami lub z wartosciami zmodyfikowanymi w podany przez nas sposob.
-Sposob modyfikacji okreslamy w funkcji ktora jest parametrem metody map()
-W naszym przypadku jest to:
-
-function(option){
-	return (<li key={option}>{option}</li>);
-}
-
-Gdzie:
-parametr 'option' to dowolna nazwa do ktorej przypiszemy wartosc elementu tablicy.
-
-Jak widzimy w powyzszymm przykladzie, funkcja zwraca element <li> posiadajacy wartosc elementu tablicy.
-Chcac zapetlic dzialane funkcji i zwrot wszystkich wartosci tablicy, musimy okreslic atrybut 'key' z unikalna wartoscia.
-Jest to wewnetrzny wymog Reacta.
-W naszym przykladzie przypisujemy wartosc elementu tablicy. To rozwiazanie nie jest wlasciwe poniewaz tablica moze zawierac elementy o takich samych wartosciach. Do naszego cwiczenia, jednak, wystarczy.
-
-
-
-
-
-
-
-##################### objasnienia poprzedniego cwiczenia #########################
-
-Aby 'ozywic' nasza forme uzyjemy zdarzenia onSubmit i za jego pomoca odwolamy sie do funkcji onFormSubmit.
-Nie wywolujemy funkcji a jedynie odwolujemy sie do niej.
-
-e.preventDefault() w funkcji sluzy do zignorowania zachowania domyslnego zdarzenia onSubmit.
-Dzieki temu zdarzeniowi mozemy przypisac wlasny kod.
-
-W pierwszej kolejnosci chcemy pobrac dane jezeli jakies zostana wprowadzone przez urzytkownika.
-posluzy nam do tego nastepujacy kod:
-const option = e.target.elements.option.value;
-
-gdzie:
-const option - jest stala przechowujaca wartosc podana przez urzytkownika
-
-e.target - odnosi sie do elementu na ktorym nasze zdzrzenie rozpoczyna dzialanie (w naszym przypadku element form)
-
-e.target.elements - odpowiada za dostep do wszystkich elementow formy posiadajacych wlasciwosc 'name'
-
-e.target.elements.option - odwolanie do elementu formy z atrybutem name="option" (w naszym przypadku jest to okno input)
-
-const option = e.target.elements.option.value - pobranie wartosci z okna input i przypisanie jej do stalej option
-
---------------------------------------------------------------------------------------------------------------------------------
-
-Nastepnie w naszej funkcji onFormSubmit deklarujemy if statement.
-
-if (option) {
-		app.options.push(option);
-		e.target.elements.option.value = '';
-		render();
-	}
-	console.log(app.options);
-}
-
-Gdzie:
-if(option) - jezeli zostala wprowadzona  wartosc 
-
-app.options.push(option) - dodajemy ja do tablicy options znajdujacej sie w obiekcie app
-
-e.target.elements.option.value = '' - usuwamy wartosc z okna input po wprowadzeniu jej do tablicy
-
-render() - odpowiada za ponowne renderowanie strony
+Zadaniem tego cwiczenia jest 
 
 */
