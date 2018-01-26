b8861d6b76f120763d5766e1199ea2f01a25cdc0
Dariusz Franczak <darius32@op.pl>
2018-01-26 00:32:29 +0000

    14a completed

--- complete-react-web-developer/projects/indecision-app/public/scripts/app.js
+++ complete-react-web-developer/projects/indecision-app/public/scripts/app.js
@@ -8,8 +8,106 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var IndecisionApp = function (_React$Component) {
-	_inherits(IndecisionApp, _React$Component);
+var Component1 = function (_React$Component) {
+	_inherits(Component1, _React$Component);
+
+	function Component1() {
+		_classCallCheck(this, Component1);
+
+		return _possibleConstructorReturn(this, (Component1.__proto__ || Object.getPrototypeOf(Component1)).apply(this, arguments));
+	}
+
+	_createClass(Component1, [{
+		key: 'render',
+		value: function render() {
+			return React.createElement(
+				'div',
+				null,
+				React.createElement(
+					'h1',
+					null,
+					'Component 1'
+				),
+				React.createElement(
+					'h3',
+					null,
+					this.props.subtitle1
+				)
+			);
+		}
+	}]);
+
+	return Component1;
+}(React.Component);
+
+var Component2 = function (_React$Component2) {
+	_inherits(Component2, _React$Component2);
+
+	function Component2() {
+		_classCallCheck(this, Component2);
+
+		return _possibleConstructorReturn(this, (Component2.__proto__ || Object.getPrototypeOf(Component2)).apply(this, arguments));
+	}
+
+	_createClass(Component2, [{
+		key: 'render',
+		value: function render() {
+			return React.createElement(
+				'div',
+				null,
+				React.createElement(
+					'h1',
+					null,
+					'Component 2'
+				),
+				React.createElement(
+					'h3',
+					null,
+					this.props.subtitle2
+				)
+			);
+		}
+	}]);
+
+	return Component2;
+}(React.Component);
+
+var Component3 = function (_React$Component3) {
+	_inherits(Component3, _React$Component3);
+
+	function Component3() {
+		_classCallCheck(this, Component3);
+
+		return _possibleConstructorReturn(this, (Component3.__proto__ || Object.getPrototypeOf(Component3)).apply(this, arguments));
+	}
+
+	_createClass(Component3, [{
+		key: 'render',
+		value: function render() {
+			return React.createElement(
+				'div',
+				null,
+				React.createElement(
+					'h1',
+					null,
+					'Component 3'
+				),
+				React.createElement(
+					'h3',
+					null,
+					'Hello ',
+					this.props.personName,
+					'!'
+				)
+			);
+		}
+	}]);
+
+	return Component3;
+}(React.Component);
+
+var IndecisionApp = function (_React$Component4) {
+	_inherits(IndecisionApp, _React$Component4);
 
 	function IndecisionApp() {
 		_classCallCheck(this, IndecisionApp);
@@ -20,13 +118,15 @@ var IndecisionApp = function (_React$Component) {
 	_createClass(IndecisionApp, [{
 		key: 'render',
 		value: function render() {
-			var actionTitle = 'Action Title from variable';
+			var subtitle2 = 'Subtitle 2';
+			var name = 'Mark';
 
 			return React.createElement(
 				'div',
 				null,
-				React.createElement(Header, { title1: 'Header Title' }),
-				React.createElement(Action, { title2: actionTitle })
+				React.createElement(Component1, { subtitle1: 'Subtitle 1' }),
+				React.createElement(Component2, { subtitle2: subtitle2 }),
+				React.createElement(Component3, { personName: name })
 			);
 		}
 	}]);
@@ -34,69 +134,11 @@ var IndecisionApp = function (_React$Component) {
 	return IndecisionApp;
 }(React.Component);
 
-var Header = function (_React$Component2) {
-	_inherits(Header, _React$Component2);
-
-	function Header() {
-		_classCallCheck(this, Header);
-
-		return _possibleConstructorReturn(this, (Header.__proto__ || Object.getPrototypeOf(Header)).apply(this, arguments));
-	}
-
-	_createClass(Header, [{
-		key: 'render',
-		value: function render() {
-			return React.createElement(
-				'div',
-				null,
-				React.createElement(
-					'h1',
-					null,
-					'Header Component'
-				),
-				React.createElement(
-					'h3',
-					null,
-					this.props.title1
-				)
-			);
-		}
-	}]);
-
-	return Header;
-}(React.Component);
-
-var Action = function (_React$Component3) {
-	_inherits(Action, _React$Component3);
-
-	function Action() {
-		_classCallCheck(this, Action);
-
-		return _possibleConstructorReturn(this, (Action.__proto__ || Object.getPrototypeOf(Action)).apply(this, arguments));
-	}
-
-	_createClass(Action, [{
-		key: 'render',
-		value: function render() {
-			return React.createElement(
-				'div',
-				null,
-				React.createElement(
-					'h1',
-					null,
-					'Action Component'
-				),
-				React.createElement(
-					'h3',
-					null,
-					this.props.title2
-				)
-			);
-		}
-	}]);
-
-	return Action;
-}(React.Component);
-
 var appRoot = document.getElementById('app');
 ReactDOM.render(React.createElement(IndecisionApp, null), appRoot);
+
+/*
+Właściwości (props) są używane do przekazywania dodatkowych danych do komponentów.
+Właściwości są definiowane jako atrybuty elementów HTML.
+
+*/
--- complete-react-web-developer/projects/indecision-app/src/app.js
+++ complete-react-web-developer/projects/indecision-app/src/app.js
@@ -1,42 +1,59 @@
-
-class IndecisionApp extends React.Component {
+class Component1 extends React.Component {
 	render() {
-		let actionTitle = 'Action Title from variable';
-
 		return (
 			<div>
-				<Header title1='Header Title'/>
-				<Action title2={actionTitle}/>
+				<h1>Component 1</h1>
+				<h3>{this.props.subtitle1}</h3>
 			</div>
 		);
 	} 
 }
 
 
-class Header extends React.Component {
+class Component2 extends React.Component {
 	render() {
 		return (
 			<div>
-				<h1>Header Component</h1>
-				<h3>{this.props.title1}</h3>
-			</div>
-		);
-	} 
-}
-
-
-class Action extends React.Component {
-	render() {
-		return (
-			<div>
-				<h1>Action Component</h1>
-				<h3>{this.props.title2}</h3>
+				<h1>Component 2</h1>
+				<h3>{this.props.subtitle2}</h3>
 			</div>
 		);
 	}
 }
 
 
+class Component3 extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Component 3</h1>
+				<h3>Hello {this.props.personName}!</h3>
+			</div>
+		);
+	}
+}
+
+
+class IndecisionApp extends React.Component {
+	render() {
+		let subtitle2 = 'Subtitle 2';
+		let name = 'Mark'
+
+		return (
+			<div>
+				<Component1 subtitle1='Subtitle 1'/>
+				<Component2 subtitle2={subtitle2}/>
+				<Component3 personName={name}/>
+			</div>
+		);
+	} 
+}
 
 const appRoot = document.getElementById('app');
 ReactDOM.render(<IndecisionApp />, appRoot);
+
+/*
+Właściwości (props) są używane do przekazywania dodatkowych danych do komponentów.
+Właściwości są definiowane jako atrybuty elementów HTML.
+
+*/
